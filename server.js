a013ae30b131cc2e6c8b63aeded6f151e95eebbd
gokulc1094 <30663639+gokulc1094@users.noreply.github.com>
2017-08-04 21:06:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
-var articleone={
+var articles={
+     articleone:{
     title:"article one..webb",
     heading:"article one",
     date:"aug 3 2017",
@@ -31,6 +31,57 @@ var articleone={
         cvlmkmklvmc
         vbknmcxlkvc
     </p>`
+},
+     articletwo:{
+    
+     title:"article two..webb",
+    heading:"article two",
+    date:"aug 6 2017",
+    content:` <p>
+        
+        These arte the contents of article oneeee..djvnndnvndndkm, kcvmklmxlvklv,cv Vc
+        cvlmkmklvmc
+        vbknmcxlkvc
+    </p>
+     <p>
+        
+        These arte the contents of article oneeee..djvnndnvndndkm, kcvmklmxlvklv,cv Vc
+        cvlmkmklvmc
+        vbknmcxlkc
+    </p>
+     <p>
+        
+        These arte the contents of article oneeee..djvnndnvndndkm, kcvmklmxlvklv,cv Vc
+        cvlmkmklvmc
+        vbknmcxlkvc
+    </p>`
+    
+},
+     articlethree:{
+    
+    
+     title:"article three..webb",
+    heading:"article three",
+    date:"aug 9 2017",
+    content:` <p>
+        
+        These arte the contents of article oneeee..djvnndnvndndkm, kcvmklmxlvklv,cv Vc
+        cvlmkmklvmc
+        vbknmcxlkvc
+    </p>
+     <p>
+        
+        These arte the contents of article oneeee..djvnndnvndndkm, kcvmklmxlvklv,cv Vc
+        cvlmkmklvmc
+        vbknmcxlkc
+    </p>
+     <p>
+        
+        These arte the contents of article oneeee..djvnndnvndndkm, kcvmklmxlvklv,cv Vc
+        cvlmkmklvmc
+        vbknmcxlkvc
+    </p>`
+},
 };
 
  function createtemplate(data)
