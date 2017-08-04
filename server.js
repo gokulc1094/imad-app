b5a34ea139030eeeb82771ced477683bf513d5f2
gokulc1094 <30663639+gokulc1094@users.noreply.github.com>
2017-08-04 19:09:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,11 +15,11 @@ app.get('/article-one', function (req, res) {
 });
 
 app.get('/article-two', function (req, res) {
-  res.send('article two is loaded');
+ res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
 app.get('/article-three', function (req, res) {
-  res.send('article three is loaded');
+ res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
 
