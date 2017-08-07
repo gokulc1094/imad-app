58c8ce402120c6498f79cf467349205118fbfd18
gokulc1094 <30663639+gokulc1094@users.noreply.github.com>
2017-08-08 01:38:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -29,27 +29,46 @@ button.onclick=function(){
      request.open('GET','http://gokulc1094.imad.hasura-app.io/counter',true);
      request.send(null);
 };
+    //name request
+    
     
   var nameInput=document.getElementById('name');
   var name=nameInput.value;
   var submit=document.getElementById('submit-btn');
-  submit.onclick=function(){
-      var names=['name1','name2','name3','name4'];
-      var list='';
-      for(var i=0;i<names.length;i++)
-      {
-          list+='<li>'+names[i]+'</li>';
-          
-      }
+  submit.onclick=function()
+  {
+      
+       //create or make an an http request
+      var request=new XMLHttpRequest();
+    
+    //recive response and respond
+     request.onreadystatechange=function()
+     {
+        if(request.readyState===XMLHttpRequest.DONE)
+         {
+              if(request.status===200)
+              {
+                var names=request.responseText;
+                names=JSON.parse(names);
+                var list='';
+                for(var i=0;i<names.length;i++)
+                 {
+                   list+='<li>'+names[i]+'</li>';
+                 }
   
     var ul=document.getElementById('namelist');
     ul.innerHTML=list;
 
+
+              }
+         }
+ 
  
   };
     
     
-    
+    request.open('GET','http://gokulc1094.imad.hasura-app.io/submit-one'+name,true);
+     request.send(null);
     //render 
     
     
