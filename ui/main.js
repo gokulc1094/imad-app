de3fb7c027b03535f03b231d3f80f1200c1d1e02
gokulc1094 <30663639+gokulc1094@users.noreply.github.com>
2017-08-07 21:28:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,10 @@
 //counter
 var counter=0;
-var button =document.getElementById("button");
+var button =document.getElementById('button');
 button.onClick=function(){
     //render 
     
     counter=counter+1;
-    var span=document.getElementById("count");
-    span.InnerHTML=counter.toString();
+    var span=document.getElementById('count');
+    span.innerHTML=counter.toString();
 };
