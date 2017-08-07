6ccc8586cf54a9409b910228ae46a2c31dd9dc48
gokulc1094 <30663639+gokulc1094@users.noreply.github.com>
2017-08-08 00:40:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,7 +30,23 @@ button.onclick=function(){
      request.send(null);
 };
     
-    
+  var nameinput=document.getElementById('name');
+  var name=nameinput.value;
+  var button=document.getElementById('submit-btn');
+  button.onclick=function(){
+      var names=['name1','name2','name3','name4'];
+      var list='';
+      for(var i=0;i<names.length;i++)
+      {
+          list=list+'<li>'+names[i]+'</li>';
+          
+      }
+  
+    var ul=document.getElementById('namelist');
+    ul.innerHTMl=list;
+
+ 
+  };
     
     
     
