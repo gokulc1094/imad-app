d2b92c9ec105b5f3981191f2d1ea9d64b82e2651
gokulc1094 <30663639+gokulc1094@users.noreply.github.com>
2017-08-07 14:40:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,6 +6,14 @@ console.log('Loaded!');
   
 //image move
 var img=document.getElementById("modi");
+var marginLeft=0;
+function moveRight()
+{
+     marginLeft=marginLeft+10;
+     img.style.marginLeft=marginLeft+"px";
+}
+
 img.onclick=function(){
-    img.style.marginLeft="100px";
+    var interval=setInterval(moveRight,50);
+   
 };
