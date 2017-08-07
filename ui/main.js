9892a40f619fcb1b7968caa449f908cd7c5679a4
gokulc1094 <30663639+gokulc1094@users.noreply.github.com>
2017-08-07 23:45:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,39 @@
 //counter
 var button =document.getElementById('button');
-var counter=0;
+
 
 button.onclick=function(){
+    
+    //create or make an an http request
+      var request=new XMLHttpRequest();
+    
+    //recive response and respond
+     request.onreadystatechange=function(){
+         
+         if(request.readystate===XMLHttpRequest.DONE)
+         {
+              if(request.status===200)
+              {
+                   var counter=request.responseText;
+                   var span=document.getElementById('count');
+                   span.innerHTML=counter.toString();
+
+              }
+         }
+         
+         
+     };
+     
+     //make the request
+     
+     request.open('GET','http://gokulc1094.imad.hasura-app.io/counter',true);
+     request.send(null);
+};
+    
+    
+    
+    
+    
     //render 
     
-    counter=counter+1;
-    var span=document.getElementById('count');
-    span.innerHTML=counter.toString();
-};
+    
