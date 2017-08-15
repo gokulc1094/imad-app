ffe4f84be9488b8778ad2a81814f8ec17edc74fc
gokulc1094 <30663639+gokulc1094@users.noreply.github.com>
2017-08-15 14:11:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -161,7 +161,7 @@ app.get('/:articlename', function (req, res) {
 
 var pool=new Pool(config);
 app.get("/test-db",function(req,res){
-    pool.query('SELECT * FROM test',function(err,result)
+    pool.query('SELECT * FROM test',function(err,result){
     if(err)
      {
            res.status(500).send(err.toString());
