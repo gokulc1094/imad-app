2ce8c649f7af5d0225f76d69d13ce329362c8671
gokulc1094 <30663639+gokulc1094@users.noreply.github.com>
2017-08-16 14:56:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -130,7 +130,7 @@ var articles={
                         </div>
                         <div>
                         <p>
-                        ${date}
+                        ${date.toDateString}
                         </p>
                        ${content}
                           </div>    
