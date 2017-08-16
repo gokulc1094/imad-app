2e092b8ab39131113fcf9a2b0cd6af64e021a2a2
gokulc1094 <30663639+gokulc1094@users.noreply.github.com>
2017-08-16 14:16:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -165,9 +165,26 @@ app.get("/test-db",function(req,res){
 });
 
 
-app.get('/:articlename', function (req, res) {
-    var articlename=req.params.articlename;
- res.send(createtemplate(articles[articlename]));
+app.get('/articles/:articlename', function (req, res) {
+    
+    pool.query("SELECT * FROM article WHERe title="+req.params.articleName+function(err,result){
+      if(err)
+     {
+           res.status(500).send(err.toString());
+     }    
+     else if(result.legth.rows===0)
+          {
+              res.status(404).send('article not found');
+          }
+        
+     else{
+          var articledata=result.rows[0];
+           res.send(createtemplate(articledata)); 
+         }
+    
+    });
+  
+
 });
 
 
